cd6bef30d59296392e8a9c7ee048d78de41e68e5
vani-231 <vani630145@gmail.com>
2022-01-12 21:20:59 +0530

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ import { Update } from '../styles/GlobalComponents';
 const Home = () => {
   return (
     <Layout>
-      <Update grid>
+      <Update>
         <Section>
           <Hero />
           <BgAnimation />
