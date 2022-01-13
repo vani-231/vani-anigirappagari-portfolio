8eaf7019bb1df25a42dde46d9efe8effc6dc02dd
vani-231 <vani630145@gmail.com>
2022-01-13 16:18:08 +0530

    .

--- src/components/BackgrooundAnimation/BackgroundAnimation.js
+++ src/components/BackgrooundAnimation/BackgroundAnimation.js
@@ -370,7 +370,7 @@ const BackgroundAnimation = () => (
       top: 100,
       left: 150
     }}>
-      <img src='./images/mypic.jpeg' alt="vani" style={{ display: "flex", height: "100%", width: "100%", borderRadius: "50%" }} />
+      <img src='./images/mypic.jpeg' alt="vani" style={{ height: "100%", width: "100%", borderRadius: "50%" }} />
     </div>
   </div>
 );
