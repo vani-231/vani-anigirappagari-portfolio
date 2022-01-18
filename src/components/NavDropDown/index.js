efeb0eab0a1e9cbafaefe9207d2943206e155bb4
vani-231 <vani630145@gmail.com>
2022-01-18 12:52:19 +0530

    .

--- src/components/NavDropDown/index.js
+++ src/components/NavDropDown/index.js
@@ -6,9 +6,7 @@ import { DropDownContainer, DropDownIcon, DropDownItem, DropDownItemDesc, DropDo
 
 const NavDropDown = (props) => (
   <div>
-    <DropDownContainer>
 
-    </DropDownContainer>
     NavDropDown
   </div>
 );
