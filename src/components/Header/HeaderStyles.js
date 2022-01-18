e7e5219d250625d3a14eca5f891dd0866ffafc08
vani-231 <vani630145@gmail.com>
2022-01-18 18:11:16 +0530

    .

--- src/components/Header/HeaderStyles.js
+++ src/components/Header/HeaderStyles.js
@@ -102,6 +102,39 @@ export const ContactDropDown = styled.button`
   }
 `;
 
+export const ProjectsDropDown = styled.button`
+  border: none;
+  display: flex;
+  position: relative;
+  background: none;
+  font-size: 1.7rem;
+
+  line-height: 32px;
+  color: rgba(255, 255, 255, 0.75);
+  cursor: pointer;
+  transition: 0.3s ease;
+
+  &:focus {
+    outline: none;
+  }
+  &:hover {
+    color: #fff;
+  }
+
+  @media ${(props) => props.theme.breakpoints.sm} {
+    padding: 0.4rem 0;
+  }
+  @media ${(props) => props.theme.breakpoints.md} {
+    padding: 0;
+  }
+`;
+
+
+
+
+
+
+
 export const NavProductsIcon = styled(IoIosArrowDropdown)`
   margin-left: 8px;
   display: flex;
