d25b28a0d317f1f5efa4939b3a0c30cc42152ded
vani-231 <vani630145@gmail.com>
2022-01-18 11:53:01 +0530

    .

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,41 +4,53 @@ import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, TagL
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { projects } from '../../constants/constants';
 import TechIcons from './TechIcons'
+import {
+  Container,
+  TextContainer,
+  Label,
+  Input,
+  TextArea,
+  FormHeading,
+  Submit,
+  ButtonDiv,
+} from "./ContactStyles";
 const Projects = () => (
-  <Section nopadding id='projects'>
-    <SectionDivider />
-    <br />
-    <SectionTitle>Projects</SectionTitle>
+  <Container>
+    <Section nopadding id='projects'>
+      <SectionDivider />
+      <br />
+      <SectionTitle>Projects</SectionTitle>
 
-    <GridContainer>
-      {projects.map((project) => (
-        <BlogCard key={project.id}>
-          <Img src={project.image} />
-          <TitleContent>
-            <HeaderThree title>{project.title}</HeaderThree>
-            <Hr />
-          </TitleContent>
-          <CardInfo>{project.description}</CardInfo>
-          <div>
-            <TitleContent>Tech Stack</TitleContent>
-            <TagList>
-              {project.tags.map((tag, i) => {
-                return <div style={{ textAlign: 'center' }}>
-                  <TechIcons key={i} tag={tag} />
-                  <div>{tag}</div>
-                </div>
+      <GridContainer>
+        {projects.map((project) => (
+          <BlogCard key={project.id}>
+            <Img src={project.image} />
+            <TitleContent>
+              <HeaderThree title>{project.title}</HeaderThree>
+              <Hr />
+            </TitleContent>
+            <CardInfo>{project.description}</CardInfo>
+            <div>
+              <TitleContent>Tech Stack</TitleContent>
+              <TagList>
+                {project.tags.map((tag, i) => {
+                  return <div style={{ textAlign: 'center' }}>
+                    <TechIcons key={i} tag={tag} />
+                    <div>{tag}</div>
+                  </div>
 
-              })}
-            </TagList>
-          </div>
-          <UtilityList>
-            <ExternalLinks href={project.visit} target='_blank'>VISIT</ExternalLinks>
-            <ExternalLinks href={project.source} target='_blank'>CODE</ExternalLinks>
-          </UtilityList>
-        </BlogCard>
-      ))}
-    </GridContainer>
-  </Section>
+                })}
+              </TagList>
+            </div>
+            <UtilityList>
+              <ExternalLinks href={project.visit} target='_blank'>VISIT</ExternalLinks>
+              <ExternalLinks href={project.source} target='_blank'>CODE</ExternalLinks>
+            </UtilityList>
+          </BlogCard>
+        ))}
+      </GridContainer>
+    </Section>
+  </Container>
 );
 
 export default Projects;
