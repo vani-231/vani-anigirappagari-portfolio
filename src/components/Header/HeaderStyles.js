9193cef17e9650381c1ba1951788e81c3d00e02e
vani-231 <vani630145@gmail.com>
2022-01-18 15:49:57 +0530

    .

--- src/components/Header/HeaderStyles.js
+++ src/components/Header/HeaderStyles.js
@@ -11,8 +11,9 @@ export const Container = styled.div`
   padding-top: 2rem;
   background-color: #rgb(17, 24, 37);
   box-shadow: #rgb(36, 36, 58) 3px 3px 10px;"
-  position:relative;
-  
+  position:static;
+  backface-visibility: hidden;
+
 
   
   
